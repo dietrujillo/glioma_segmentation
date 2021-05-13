d10d940c78e6f4f430bbeb6a8653d54601a03306
Diego Trujillo <dtrujilloj@protonmail.com>
2021-05-13 09:34:30 +0000

    Remove loss weights

--- training/loss.py
+++ training/loss.py
@@ -2,9 +2,6 @@ import tensorflow as tf
 
 from definitions import PREPROCESSED_DATA_SHAPE, LOSS_WEIGHTS
 
-LOSS_WEIGHTS_TENSOR = tf.stack([tf.fill(PREPROCESSED_DATA_SHAPE, loss_weight)
-                                for loss_weight in LOSS_WEIGHTS], axis=-1)
-
 
 def dice_loss(y_true: tf.Tensor, y_pred: tf.Tensor, epsilon: float = 1e-10) -> tf.Tensor:
     """
@@ -22,5 +19,4 @@ def dice_loss(y_true: tf.Tensor, y_pred: tf.Tensor, epsilon: float = 1e-10) -> t
     denominator = tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) + epsilon
     general_loss = tf.constant(1.) - tf.constant(2.) * tf.divide(numerator, denominator)
     per_pixel_loss = general_loss * tf.square(tf.subtract(y_true, y_pred))
-    loss_weights = tf.stack([LOSS_WEIGHTS_TENSOR] * y_true.shape[0], axis=0)
-    return tf.multiply(per_pixel_loss, loss_weights)
+    return per_pixel_loss
